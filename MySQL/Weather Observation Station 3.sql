b3e208c635415eebf62aba5980b791d8215e7285
Putu Aryasuta Wicaksana <54010325+AryasutaWicaksana@users.noreply.github.com>
2019-09-09 15:23:03 +0800

    Create Weather Observation Station 3.sql

---
+++ MySQL/Weather Observation Station 3.sql
@@ -0,0 +1,18 @@
+/* INPUT
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+Query a list of CITY names from STATION with even ID numbers only. 
+You may print the results in any order, but must exclude duplicates from your answer.
+*/
+
+SELECT DISTINCT CITY FROM STATION WHERE (ID%2=0)
+
+// distinct -> to remove duplicate data
+// ```where (id%2=0)``` to set condition and get only the even ID
