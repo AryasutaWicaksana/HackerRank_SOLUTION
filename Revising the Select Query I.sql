8cc4cf9a232061aa0975c546986fbe706fd18de7
Putu Aryasuta Wicaksana <54010325+AryasutaWicaksana@users.noreply.github.com>
2019-09-09 14:35:48 +0800

    Update Revising the Select Query I.sql

--- Revising the Select Query I.sql
+++ Revising the Select Query I.sql
@@ -1,3 +1,16 @@
-
+/* this query to select all columns for all American Cities In CITY
+  with populations larger than 100000
+*/
+/* INPUT
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+*/
 SELECT* FROM CITY WHERE POPULATION>100000 AND COUNTRYCODE = "USA"
 
