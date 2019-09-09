4f59b603c917ced17827dced82582e3b6e68035b
Putu Aryasuta Wicaksana <54010325+AryasutaWicaksana@users.noreply.github.com>
2019-09-09 15:17:35 +0800

    Create Weather Observation Station 1.sql

---
+++ MySQL/Weather Observation Station 1.sql
@@ -0,0 +1,16 @@
+/* INPUT
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMBER     |
+| LONG_W      | NUMBER     |
++-------------+------------+
+Query a list of CITY and STATE from the STATION table.
+*/
+/*
+Enter your query here.
+*/
+select CITY,STATE from STATION 
