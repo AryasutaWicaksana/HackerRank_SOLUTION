f56487c1f542b357e2b289411f1fbad4941e6626
Putu Aryasuta Wicaksana <54010325+AryasutaWicaksana@users.noreply.github.com>
2019-09-09 15:26:34 +0800

    Create Weather Observation Station 4.sql

---
+++ MySQL/Weather Observation Station 4.sql
@@ -0,0 +1,16 @@
+/* INPUT
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | Number	   |
+| LONG_W      | Number	   |
++-------------+------------+
+
+Let N be the number of CITY entries in STATION, and let N' be the number of distinct CITY names in STATION;
+query the value of (N - N')  from STATION. In other words, 
+find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
+*/
+SELECT (COUNT(CITY)-COUNT(DISTINCT CITY)) from STATION
