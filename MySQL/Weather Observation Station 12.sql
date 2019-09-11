5ad2575908a8bb108fd2b5ba885ecaca63ab61db
Putu Aryasuta Wicaksana <54010325+AryasutaWicaksana@users.noreply.github.com>
2019-09-11 15:58:51 +0800

    Create Weather Observation Station 12.sql

---
+++ MySQL/Weather Observation Station 12.sql
@@ -0,0 +1,15 @@
+/* INPUT
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | Number	   |
+| LONG_W      | Number	   |
++-------------+------------+
+Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. 
+Your result cannot contain duplicates.*/
+Select distinct CITY from STATION where
+not left(CITY,1) in ('a','i','u','e','o') and
+not right(CITY,1) in ('a','i','u','e','o')
