f76d76bd092eaad08232eb2b21d578e166ebaa12
Putu Aryasuta Wicaksana <54010325+AryasutaWicaksana@users.noreply.github.com>
2019-09-11 15:50:54 +0800

    Create Weather Observation Station 6.sql

---
+++ MySQL/Weather Observation Station 6.sql
@@ -0,0 +1,14 @@
+/* INPUT
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | Number	   |
+| LONG_W      | Number	   |
++-------------+------------+
+Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+*/
+
+select distinct CITY from STATION WHERE CITY LIKE 'a%' OR CITY LIKE'i%' OR CITY LIKE 'u%' OR CITY LIKE 'e%' OR CITY LIKE 'o%'
